4e74225446eda90cda32ba6ec52abb0266fe8c74
ScoobyPlsDont <joshualeskowsky9@gmail.com>
2026-07-21 20:56:24 -0500

    Update yeeps-api-python.py

--- yeeps-api-python.py
+++ yeeps-api-python.py
@@ -1,9 +1,6 @@
 def api_call(oculusID, mobileCode):
     url = "https://yeeps-proxy.onrender.com/yeeps"
-    body = {
-        "oculusID": oculusID,
-        "mobileCode": mobileCode
-    }
+    body = {"oculusID": oculusID, "mobileCode": mobileCode}
     try:
         response = requests.post(url, json=body, timeout=120)
         response.raise_for_status()
